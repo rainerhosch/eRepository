94bd37fcfc134bc1bf6f2f6bd6a416653a38f142
yrzoktan <rz.oktan@gmail.com>
2022-08-11 12:08:23 +0700

    fix bug update password

--- application/views/layout/topbar.php
+++ application/views/layout/topbar.php
@@ -1,76 +1,76 @@
-<div class="navbar-custom">
-    <ul class="list-unstyled topnav-menu float-end mb-0">
-        <li class="dropdown d-inline-block d-lg-none">
-            <a class="nav-link dropdown-toggle arrow-none waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                <i class="fe-search noti-icon"></i>
-            </a>
-            <div class="dropdown-menu dropdown-lg dropdown-menu-end p-0">
-                <form class="p-3">
-                    <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username">
-                </form>
-            </div>
-        </li>
-        <li class="dropdown notification-list topbar-dropdown">
-            <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                <img src="<?= base_url('assets/template') ?>/images/users/user-1.jpg" alt="user-image" class="rounded-circle">
-                <span class="pro-user-name ms-1">
-                    <?= $this->session->userdata('fullname'); ?> <i class="mdi mdi-chevron-down"></i>
-                </span>
-            </a>
-            <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
-                <?php if ($this->session->userdata('role') === '1' || $this->session->userdata('role') === '2') : ?>
-                    <a href="#profile" class="dropdown-item notify-item btn_edit_profile">
-                        <i class="fe-user"></i>
-                        <span>Edit Profile</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                <?php endif; ?>
-                <a href="<?= base_url('auth'); ?>/logout" class="dropdown-item notify-item">
-                    <i class="fe-log-out"></i>
-                    <span>Logout</span>
-                </a>
-            </div>
-        </li>
-    </ul>
-    <ul class="list-unstyled topnav-menu topnav-menu-left mb-0">
-        <li>
-            <button class="button-menu-mobile disable-btn waves-effect">
-                <i class="fe-menu"></i>
-            </button>
-        </li>
-        <li>
-            <h4 class="page-title-main"><?php
-                                        echo $page;
-                                        if (isset($subpage)) {
-                                            echo '<i class="mdi mdi-triangle-wave"></i>' . $subpage;
-                                        }; ?>
-            </h4>
-        </li>
-    </ul>
-    <!-- LOGO -->
-    <div class="logo-box">
-        <h3 href="#" class="logo text-center" style="margin:0px; color:#fff;">
-            <span class="logo-sm">
-                <!-- <img src="<?= base_url('assets/template') ?>/images/logo-sm.png" alt="" height="22"> -->
-                <strong>
-                    <i class="dripicons-store"></i>
-                </strong>
-            </span>
-            <span class="logo-lg">
-                <!-- <img src="<?= base_url('assets/template') ?>/images/logo-light.png" alt="" height="16"> -->
-                <strong height="16"><i class="dripicons-store"></i> e-Library </strong>
-            </span>
-        </h3>
-        <a href="#" class="logo logo-dark text-center">
-            <span class="logo-sm">
-                <img src="<?= base_url('assets/template') ?>/images/logo-sm.png" alt="" height="22">
-            </span>
-            <span class="logo-lg">
-                <img src="<?= base_url('assets/template') ?>/images/logo-dark.png" alt="" height="16">
+<div class="navbar-custom"></div>
+<ul class="list-unstyled topnav-menu float-end mb-0">
+    <li class="dropdown d-inline-block d-lg-none">
+        <a class="nav-link dropdown-toggle arrow-none waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+            <i class="fe-search noti-icon"></i>
+        </a>
+        <div class="dropdown-menu dropdown-lg dropdown-menu-end p-0">
+            <form class="p-3">
+                <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username">
+            </form>
+        </div>
+    </li>
+    <li class="dropdown notification-list topbar-dropdown">
+        <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+            <img src="<?= base_url('assets/template') ?>/images/users/user-1.jpg" alt="user-image" class="rounded-circle">
+            <span class="pro-user-name ms-1">
+                <?= $this->session->userdata('fullname'); ?> <i class="mdi mdi-chevron-down"></i>
             </span>
         </a>
-    </div>
-    <div class="clearfix"></div>
+        <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
+            <?php if ($this->session->userdata('role') === '1' || $this->session->userdata('role') === '2') : ?>
+                <a href="#profile" class="dropdown-item notify-item btn_edit_profile">
+                    <i class="fe-user"></i>
+                    <span>Edit Profile</span>
+                </a>
+                <div class="dropdown-divider"></div>
+            <?php endif; ?>
+            <a href="<?= base_url('auth'); ?>/logout" class="dropdown-item notify-item">
+                <i class="fe-log-out"></i>
+                <span>Logout</span>
+            </a>
+        </div>
+    </li>
+</ul>
+<ul class="list-unstyled topnav-menu topnav-menu-left mb-0">
+    <li>
+        <button class="button-menu-mobile disable-btn waves-effect">
+            <i class="fe-menu"></i>
+        </button>
+    </li>
+    <li>
+        <h4 class="page-title-main"><?php
+                                    echo $page;
+                                    if (isset($subpage)) {
+                                        echo '<i class="mdi mdi-triangle-wave"></i>' . $subpage;
+                                    }; ?>
+        </h4>
+    </li>
+</ul>
+<!-- LOGO -->
+<div class="logo-box">
+    <h3 href="#" class="logo text-center" style="margin:0px; color:#fff;">
+        <span class="logo-sm">
+            <!-- <img src="<?= base_url('assets/template') ?>/images/logo-sm.png" alt="" height="22"> -->
+            <strong>
+                <i class="dripicons-store"></i>
+            </strong>
+        </span>
+        <span class="logo-lg">
+            <!-- <img src="<?= base_url('assets/template') ?>/images/logo-light.png" alt="" height="16"> -->
+            <strong height="16"><i class="dripicons-store"></i> e-Library </strong>
+        </span>
+    </h3>
+    <a href="#" class="logo logo-dark text-center">
+        <span class="logo-sm">
+            <img src="<?= base_url('assets/template') ?>/images/logo-sm.png" alt="" height="22">
+        </span>
+        <span class="logo-lg">
+            <img src="<?= base_url('assets/template') ?>/images/logo-dark.png" alt="" height="16">
+        </span>
+    </a>
+</div>
+<div class="clearfix"></div>
 </div>
 
 <!-- Modal Edit -->
@@ -148,7 +148,7 @@
                 dataType: "json",
                 success: function(response) {
                     let data_user = response.data;
-                    console.log(data_user)
+                    // console.log(data_user)
                     $('#user_id').val(data_user.user_id)
                     $('#user_detail_id').val(data_user.user_detail_id)
                     $('#nama_user').val(data_user.nama)
@@ -185,7 +185,7 @@
                 data: form_data,
                 dataType: "json",
                 success: function(response) {
-                    console.log(response);
+                    // console.log(response);
                     let icon = ``;
                     let title = ``;
                     let text = ``;
